8a1df5f7ddc16dd2c9348498ddf2a54e1528fdf6
walkerwang158 <95159733+walkerwang158@users.noreply.github.com>
2021-11-28 14:57:17 +0800

    Update blog.config.js
    Update basic setting

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'Walker's Blogs',
+  author: 'walker wang',
+  email: 'kwang158@163.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: 'Just do it for yourself.',
+  lang: 'en-US', 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
